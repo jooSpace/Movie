ca6132695838a25e8beb56adbf223635c0c5c663
juhyung <juhyung@gmail.com>
2023-04-18 20:56:33 +0900

    toFixed(1) 소수점 처리

--- src/components/boxOffiec/BoxOfficeDayList.js
+++ src/components/boxOffiec/BoxOfficeDayList.js
@@ -36,7 +36,7 @@ const BoxOfficeList = () => {
                         <img className='' src={`https://image.tmdb.org/t/p/w300${dayMovie.poster_path}`} alt={dayMovie.title} />
                         <h2 className='text-center mt-5 font-bold text-lg'>{dayMovie.title}</h2>
                         <div className='flex'>
-                            <p className='mr-3'>평점 : {dayMovie.vote_average} / 10 </p>
+                            <p className='mr-3'>평점 : {dayMovie.vote_average.toFixed(1)} / 10 </p>
                         </div>
                         <StarRatings
                             svgIconPath='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
@@ -57,7 +57,7 @@ const BoxOfficeList = () => {
                             <img className='' src={`https://image.tmdb.org/t/p/w300${dayMovie.poster_path}`} alt={dayMovie.title} />
                             <h2 className='text-center mt-5 font-bold text-lg'>{dayMovie.title}</h2>
                             <div className='flex'>
-                                <p className='mr-3'>평점 : {dayMovie.vote_average} / 10 </p>
+                                <p className='mr-3'>평점 : {dayMovie.vote_average.toFixed(1)} / 10 </p>
                             </div>
                             <StarRatings
                                 svgIconPath='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
@@ -78,7 +78,7 @@ const BoxOfficeList = () => {
                             <img className='' src={`https://image.tmdb.org/t/p/w300${dayMovie.poster_path}`} alt={dayMovie.title} />
                             <h2 className='text-center mt-5 font-bold text-lg'>{dayMovie.title}</h2>
                             <div className='flex'>
-                                <p className='mr-3'>평점 : {dayMovie.vote_average} / 10 </p>
+                                <p className='mr-3'>평점 : {dayMovie.vote_average.toFixed(1)} / 10 </p>
                             </div>
                             <StarRatings
                                 svgIconPath='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
@@ -98,7 +98,7 @@ const BoxOfficeList = () => {
                             <img className='' src={`https://image.tmdb.org/t/p/w300${dayMovie.poster_path}`} alt={dayMovie.title} />
                             <h2 className='text-center mt-5 font-bold text-lg'>{dayMovie.title}</h2>
                             <div className='flex'>
-                                <p className='mr-3'>평점 : {dayMovie.vote_average} / 10 </p>
+                                <p className='mr-3'>평점 : {dayMovie.vote_average.toFixed(1)} / 10 </p>
                             </div>
                             <StarRatings
                                 svgIconPath='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
